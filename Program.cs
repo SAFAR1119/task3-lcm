2ac89e6579341d40c3242ab2f32a55b74cfd889e
Safar Ahmed <safarahmed11@gmail.com>
2026-08-13 00:50:29 +0600

    Fix Render configuration watcher

--- Program.cs
+++ Program.cs
@@ -1,16 +1,24 @@
 using System.Numerics;
 
-var app = WebApplication.CreateBuilder(args).Build();
+var b = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    EnvironmentName = "Production"
+});
+
+b.Configuration.Sources.Clear();
+
+var app = b.Build();
 
 app.MapGet("/safarahmed11_gmail_com", (string? x, string? y) =>
 {
     if (!BigInteger.TryParse(x, out var a) ||
-        !BigInteger.TryParse(y, out var b) ||
-        a <= 0 || b <= 0)
+        !BigInteger.TryParse(y, out var c) ||
+        a <= 0 || c <= 0)
         return Results.Text("NaN");
 
-    var g = BigInteger.GreatestCommonDivisor(a, b);
-    return Results.Text((a / g * b).ToString());
+    var g = BigInteger.GreatestCommonDivisor(a, c);
+    return Results.Text((a / g * c).ToString());
 });
 
 app.Run();
